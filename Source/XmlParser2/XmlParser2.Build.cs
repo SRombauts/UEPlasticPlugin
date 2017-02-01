e782a762a2bfc5414ff141cf3ec5eabdb3ebbb45
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2017-02-01 04:43:59 +0100

    Upgrade Build.cs file to new UE4.16 Constructor format
    # Conflicts:
    #	Source/XmlParser2/XmlParser2.Build.cs

--- Source/XmlParser2/XmlParser2.Build.cs
+++ Source/XmlParser2/XmlParser2.Build.cs
@@ -4,7 +4,7 @@ using UnrealBuildTool;
 
 public class XmlParser2 : ModuleRules
 {
-	public XmlParser2(TargetInfo Target)
+	public XmlParser2(ReadOnlyTargetRules Target) : base(Target)
 	{
 		// Do not enforce "Include What You Use" UE4.15 policy
 		// since it does not follow the same rules for In-Engine Plugins as for Game Project Plugins,
