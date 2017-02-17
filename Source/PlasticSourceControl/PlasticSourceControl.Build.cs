7b2da76ba4e90e5e9beafd3844229c2883d84b9b
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2017-02-17 11:36:35 +0100

    Build.cs : Do not enforce "Include What You Use" UE4.15 policy
    since it does not follow the same rules for In-Engine Plugins as for Game Project Plugins, and as such prevents us to make a source code compiling as both.

--- Source/PlasticSourceControl/PlasticSourceControl.Build.cs
+++ Source/PlasticSourceControl/PlasticSourceControl.Build.cs
@@ -6,8 +6,14 @@ public class PlasticSourceControl : ModuleRules
 {
 	public PlasticSourceControl(TargetInfo Target)
 	{
+		// Do not enforce "Include What You Use" UE4.15 policy
+		// since it does not follow the same rules for In-Engine Plugins as for Game Project Plugins,
+		// and as such prevents us to make a source code compiling as both.
+		bEnforceIWYU = false;
+
 		PrivateDependencyModuleNames.AddRange(
-			new string[] {
+			new string[]
+			{
 				"Core",
 				"CoreUObject",
 				"Slate",
--- Source/XmlParser2/XmlParser2.Build.cs
+++ Source/XmlParser2/XmlParser2.Build.cs
@@ -4,15 +4,19 @@ using UnrealBuildTool;
 
 public class XmlParser2 : ModuleRules
 {
-	public XmlParser2( TargetInfo Target )
+	public XmlParser2(TargetInfo Target)
 	{
-//		PublicIncludePaths.AddRange(new string[] { "Public" });
+		// Do not enforce "Include What You Use" UE4.15 policy
+		// since it does not follow the same rules for In-Engine Plugins as for Game Project Plugins,
+		// and as such prevents us to make a source code compiling as both.
+		bEnforceIWYU = false;
+
 		PrivateDependencyModuleNames.AddRange(
-			new string[] 
-			{ 
+			new string[]
+			{
 				"Core",
 				"CoreUObject",
 			}
-			);
+		);
 	}
 }
