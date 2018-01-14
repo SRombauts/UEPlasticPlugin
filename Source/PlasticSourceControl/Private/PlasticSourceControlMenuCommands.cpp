776970d1dd0f364da29575215df664d587b371ec
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2018-01-14 20:49:44 +0100

    Reword the specific actions of the Source Control top menu

--- Source/PlasticSourceControl/Private/PlasticSourceControlMenuCommands.cpp
+++ Source/PlasticSourceControl/Private/PlasticSourceControlMenuCommands.cpp
@@ -7,9 +7,9 @@
 
 void FPlasticSourceControlMenuCommands::RegisterCommands()
 {
-	UI_COMMAND(SyncProject,		"Sync",				"Update all file in the workspace to the latest version.", EUserInterfaceActionType::Button, FInputGesture());
-	UI_COMMAND(RevertUnchanged,	"Revert Unchanged",	"Revert checked-out but unchanged files in the workspace.",	EUserInterfaceActionType::Button, FInputGesture());
-	UI_COMMAND(RevertAll,		"Revert All",		"Revert all files in the workspace to the state before they where checked out.", EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(SyncProject,		"Sync/Update Workspace",	"Update all files in the workspace to the latest version.", EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(RevertUnchanged,	"Revert Unchanged",			"Revert checked-out but unchanged files in the workspace.",	EUserInterfaceActionType::Button, FInputGesture());
+	UI_COMMAND(RevertAll,		"Revert All",				"Revert all files in the workspace to their controlled/unchanged state.", EUserInterfaceActionType::Button, FInputGesture());
 }
 
 #undef LOCTEXT_NAMESPACE
