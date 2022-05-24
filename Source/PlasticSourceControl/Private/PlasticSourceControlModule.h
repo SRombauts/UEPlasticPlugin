4f5b64ed52bf0716adb4083f885176bb2533922b
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2022-05-24 12:14:54 +0200

    Remove unused header include

--- Source/PlasticSourceControl/Private/PlasticSourceControlModule.h
+++ Source/PlasticSourceControl/Private/PlasticSourceControlModule.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleInterface.h"
-#include "Modules/ModuleManager.h"
 #include "PlasticSourceControlSettings.h"
 #include "PlasticSourceControlProvider.h"
 
