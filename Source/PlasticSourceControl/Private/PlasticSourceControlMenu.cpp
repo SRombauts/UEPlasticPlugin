SRombauts/UEPlasticPlugin

commit 530e3fb6373f05b4c8c657d972be22c7d59c756f

Fix from upstream 5.2-preview-1 https://github.com/EpicGames/UnrealEngine/commit/fdd3b878
Fix a crash in the PlasticSCM module that can occur if the engine shutdown during PreInit.

#rb PJ.Kack
#jira UE-179343
#rnx
#preflight none
#ushell-cherrypick of 24560992 by paul.chipchase

- The crash itself isn't that interesting but it does distract the user from the actual error/problem which caused the engine to shutdown during PreInit wasting time while they look into the crash instead of the original error.
- When attempting to unregister the menu extension we now call ::TryGet rather than ::Try as this will return nullptr if UToolsMenu is not loaded and will not attempt to load it.

[CL 24562725 by paul chipchase in 5.2-Preview-1 branch]

Source/PlasticSourceControl/Private/PlasticSourceControlMenu.cpp
@@ -37,6 +37,11 @@
 
 void FPlasticSourceControlMenu::Register()
 {
+	if (bHasRegistered)
+	{
+		return;
+	}
+
 	// Register the menu extension with the level editor
 #if ENGINE_MAJOR_VERSION == 4
 	if (FLevelEditorModule* LevelEditorModule = FModuleManager::GetModulePtr<FLevelEditorModule>("LevelEditor"))
@@ -45,6 +50,8 @@ void FPlasticSourceControlMenu::Register()
 		auto& MenuExtenders = LevelEditorModule->GetAllLevelEditorToolbarSourceControlMenuExtenders();
 		MenuExtenders.Add(ViewMenuExtender);
 		ViewMenuExtenderHandle = MenuExtenders.Last().GetHandle();
+
+		bHasRegistered = true;
 	}
 #elif ENGINE_MAJOR_VERSION == 5
 	FToolMenuOwnerScoped SourceControlMenuOwner("PlasticSourceControlMenu");
@@ -54,12 +61,19 @@ void FPlasticSourceControlMenu::Register()
 		FToolMenuSection& Section = SourceControlMenu->AddSection("PlasticSourceControlActions", LOCTEXT("PlasticSourceControlMenuHeadingActions", "Unity Version Control"), FToolMenuInsert(NAME_None, EToolMenuInsertType::First));
 
 		AddMenuExtension(Section);
+
+		bHasRegistered = true;
 	}
 #endif
 }
 
 void FPlasticSourceControlMenu::Unregister()
 {
+	if (!bHasRegistered)
+	{
+		return;
+	}
+
 	// Unregister the menu extension from the level editor
 #if ENGINE_MAJOR_VERSION == 4
 	if (FLevelEditorModule* LevelEditorModule = FModuleManager::GetModulePtr<FLevelEditorModule>("LevelEditor"))
@@ -67,9 +81,10 @@ void FPlasticSourceControlMenu::Unregister()
 		LevelEditorModule->GetAllLevelEditorToolbarSourceControlMenuExtenders().RemoveAll([=](const FLevelEditorModule::FLevelEditorMenuExtender& Extender) { return Extender.GetHandle() == ViewMenuExtenderHandle; });
 	}
 #elif ENGINE_MAJOR_VERSION == 5
-	if (UToolMenus* ToolMenus = UToolMenus::Get())
+	if (UToolMenus* ToolMenus = UToolMenus::TryGet())
 	{
 		UToolMenus::Get()->UnregisterOwnerByName("PlasticSourceControlMenu");
+		bHasRegistered = false;
 	}
 #endif
 }

Source/PlasticSourceControl/Private/PlasticSourceControlMenu.h
@@ -47,6 +47,10 @@ private:
 	void DisplayFailureNotification(const FName& InOperationName);
 
 private:
+
+	/** Tracks if the menu extension has been registered with the editor or not */
+	bool bHasRegistered = false;
+	
 #if ENGINE_MAJOR_VERSION == 4
 	FDelegateHandle ViewMenuExtenderHandle;
 #endif

commit 224dc0ba3257689bb0f6bbe5ecc904a88d9321c2

Fix Source Control menu extension disapearing in UE4
Fix missing FPlasticSourceControlMenu::Unregister() missing in bHasRegistered = false;

Bug introduced by 530e3fb6 Fix from upstream 5.2-preview-1 https://github.com/EpicGames/UnrealEngine/commit/fdd3b878

Fix a crash in the PlasticSCM module that can occur if the engine shutdown during PreInit.

Source/PlasticSourceControl/Private/PlasticSourceControlMenu.cpp
@@ -79,6 +79,7 @@ void FPlasticSourceControlMenu::Unregister()
 	if (FLevelEditorModule* LevelEditorModule = FModuleManager::GetModulePtr<FLevelEditorModule>("LevelEditor"))
 	{
 		LevelEditorModule->GetAllLevelEditorToolbarSourceControlMenuExtenders().RemoveAll([=](const FLevelEditorModule::FLevelEditorMenuExtender& Extender) { return Extender.GetHandle() == ViewMenuExtenderHandle; });
+		bHasRegistered = false;
 	}
 #elif ENGINE_MAJOR_VERSION == 5
 	if (UToolMenus* ToolMenus = UToolMenus::TryGet())