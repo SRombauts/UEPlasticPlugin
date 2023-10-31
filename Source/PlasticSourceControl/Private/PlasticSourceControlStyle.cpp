46c41e4c338ae8f89252b95e8ffa1985384a9b42
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2023-10-31 10:40:06 +0100

    Add a Style sheet to display the Logo on the new branches window tab

---
+++ Source/PlasticSourceControl/Private/PlasticSourceControlStyle.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2023 Unity Technologies
+
+#include "PlasticSourceControlStyle.h"
+
+#include "Framework/Application/SlateApplication.h"
+#include "Interfaces/IPluginManager.h"
+#include "Slate/SlateGameResources.h"
+#include "Styling/SlateStyleMacros.h"
+#include "Styling/SlateStyleRegistry.h"
+
+#include "PlasticSourceControlModule.h"
+
+TSharedPtr<FSlateStyleSet> FPlasticSourceControlStyle::StyleInstance = nullptr;
+
+void FPlasticSourceControlStyle::Initialize()
+{
+	if (!StyleInstance.IsValid())
+	{
+		StyleInstance = Create();
+		FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
+	}
+}
+
+void FPlasticSourceControlStyle::Shutdown()
+{
+	FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
+	ensure(StyleInstance.IsUnique());
+	StyleInstance.Reset();
+}
+
+FName FPlasticSourceControlStyle::GetStyleSetName()
+{
+	static FName StyleSetName(TEXT("PlasticSourceControlStyle"));
+	return StyleSetName;
+}
+
+const FVector2D Icon16x16(16.0f, 16.0f);
+
+TSharedRef<FSlateStyleSet> FPlasticSourceControlStyle::Create()
+{
+	TSharedRef<FSlateStyleSet> Style = MakeShareable(new FSlateStyleSet("PlasticSourceControlStyle"));
+	Style->SetContentRoot(FPlasticSourceControlModule::GetPlugin()->GetBaseDir() / TEXT("Resources"));
+
+	Style->Set("PlasticSourceControl.PluginIcon.Small", new FSlateImageBrush(FPlasticSourceControlStyle::InContent("Icon128", ".png"), Icon16x16));
+
+	return Style;
+}
+
+FString FPlasticSourceControlStyle::InContent(const FString& RelativePath, const ANSICHAR* Extension)
+{
+	auto myself = FPlasticSourceControlModule::GetPlugin();
+	check(myself.IsValid());
+	static FString ContentDir = myself->GetBaseDir() / TEXT("Resources");
+	return (ContentDir / RelativePath) + Extension;
+}
+
+void FPlasticSourceControlStyle::ReloadTextures()
+{
+	if (FSlateApplication::IsInitialized())
+	{
+		FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
+	}
+}
+
+const ISlateStyle& FPlasticSourceControlStyle::Get()
+{
+	return *StyleInstance;
+}
---
+++ Source/PlasticSourceControl/Private/PlasticSourceControlStyle.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 Unity Technologies
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Styling/SlateStyle.h"
+
+class FPlasticSourceControlStyle
+{
+public:
+	static void Initialize();
+
+	static void Shutdown();
+
+	/** reloads textures used by slate renderer */
+	static void ReloadTextures();
+
+	/** @return The Slate style set */
+	static const ISlateStyle& Get();
+
+	static FName GetStyleSetName();
+
+private:
+	static TSharedRef<class FSlateStyleSet> Create();
+
+private:
+	static FString InContent(const FString& RelativePath, const ANSICHAR* Extension);
+
+private:
+	static TSharedPtr<class FSlateStyleSet> StyleInstance;
+};
