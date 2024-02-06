1ea41af1db67d0f5e38cd0c9dc7280401394afe9
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-02-06 17:08:08 +0100

    Mutualize the 2 implementaions of FScopedTempFile to remove duplication of code (like in Upstream Unreal Engine)
    Also make the Temp- name agnostic.
    
    These are very short live temp files, eg holding the result of an XML output the time it takes to parse it.

--- Source/PlasticSourceControl/Private/ScopedTempFile.cpp
+++ Source/PlasticSourceControl/Private/ScopedTempFile.cpp
@@ -10,25 +10,21 @@
 
 FScopedTempFile::FScopedTempFile()
 {
-	Filename = FPaths::CreateTempFilename(*FPaths::ConvertRelativePathToFull(FPaths::ProjectLogDir()), TEXT("Plastic-Temp"), TEXT(".xml"));
+	Filename = FPaths::CreateTempFilename(*FPaths::ConvertRelativePathToFull(FPaths::ProjectLogDir()), TEXT("Temp-"), TEXT(".xml"));
 }
 
 FScopedTempFile::FScopedTempFile(const FString& InText)
 {
-	Filename = FPaths::CreateTempFilename(*FPaths::ConvertRelativePathToFull(FPaths::ProjectLogDir()), TEXT("Plastic-Temp"), TEXT(".txt"));
+	Filename = FPaths::CreateTempFilename(*FPaths::ConvertRelativePathToFull(FPaths::ProjectLogDir()), TEXT("Temp-"), TEXT(".txt"));
 	if (!FFileHelper::SaveStringToFile(InText, *Filename, FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM))
 	{
 		UE_LOG(LogSourceControl, Error, TEXT("Failed to write to temp file: %s"), *Filename);
 	}
 }
 
-FScopedTempFile::FScopedTempFile(const FText& InText)
+FScopedTempFile::FScopedTempFile(const FText& InText) :
+	FScopedTempFile(InText.ToString())
 {
-	Filename = FPaths::CreateTempFilename(*FPaths::ConvertRelativePathToFull(FPaths::ProjectLogDir()), TEXT("Plastic-Temp"), TEXT(".txt"));
-	if (!FFileHelper::SaveStringToFile(InText.ToString(), *Filename, FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM))
-	{
-		UE_LOG(LogSourceControl, Error, TEXT("Failed to write to temp file: %s"), *Filename);
-	}
 }
 
 FScopedTempFile::~FScopedTempFile()
