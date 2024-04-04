18fd9fa5924b17ab22556079aedea226edf8c3aa
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-04-04 17:58:36 +0200

    FPlasticSourceControlChangeset containing an array of file States, each with one Revision for Diffing (like for Files and ShelvedFiles in FPlasticSourceControlChangelist)

---
+++ Source/PlasticSourceControl/Private/PlasticSourceControlChangeset.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2024 Unity Technologies
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PlasticSourceControlState.h"
+
+class FPlasticSourceControlChangeset
+{
+public:
+	int32 ChangesetId;
+	FString CreatedBy;
+	FDateTime Date;
+	FString Comment;
+	FString Branch;
+	// Note: array of file States, each with one Revision for Diffing (like for Files and ShelvedFiles in FPlasticSourceControlChangelist)
+	TArray<TSharedRef<class FPlasticSourceControlState, ESPMode::ThreadSafe>> Files;
+
+	void PopulateSearchString(TArray<FString>& OutStrings) const
+	{
+		OutStrings.Emplace(CreatedBy);
+		OutStrings.Emplace(Comment);
+		OutStrings.Emplace(Branch);
+	}
+};
+
+typedef TSharedRef<class FPlasticSourceControlChangeset, ESPMode::ThreadSafe> FPlasticSourceControlChangesetRef;
+typedef TSharedPtr<class FPlasticSourceControlChangeset, ESPMode::ThreadSafe> FPlasticSourceControlChangesetPtr;
