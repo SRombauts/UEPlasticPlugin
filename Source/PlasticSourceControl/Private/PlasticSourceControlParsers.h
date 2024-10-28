768e171e21db732c30747d557dbe6a6928e7fc91
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-10-28 09:50:22 +0100

    Refactor PlasticSourceControlParsers::ParseProfileInfo()
    Now return all profiles in a Map (dictionnary)

--- Source/PlasticSourceControl/Private/PlasticSourceControlParsers.h
+++ Source/PlasticSourceControl/Private/PlasticSourceControlParsers.h
@@ -53,7 +53,7 @@ struct FRemoveRedundantErrors
 	FString Filter;
 };
 
-bool ParseProfileInfo(TArray<FString>& InResults, const FString& InServerUrl, FString& OutUserName);
+TMap<FString, FString> ParseProfileInfo(TArray<FString>& InResults);
 
 bool ParseWorkspaceInfo(TArray<FString>& InResults, FString& OutWorkspaceSelector, FString& OutBranchName, FString& OutRepositoryName, FString& OutServerUrl);
 
